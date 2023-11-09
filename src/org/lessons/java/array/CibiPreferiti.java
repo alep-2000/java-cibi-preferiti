6a6f2f6da715d1b629fa4d08f4e7309d915d05ac
alep-2000 <alessiopiras056@gmail.com>
2023-11-09 17:37:41 +0100

    stampo a video il risultato, snack 3 terminato

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -3,5 +3,9 @@ package org.lessons.java.array;
 public class CibiPreferiti {
 	public static void main(String[] args) {
 		String[] cibiPreferiti = {"Pizza", "Lasagne", "Fiorentina", "Tiramisù", "Cioccolata"};
+		
+		System.out.println("La lunghezza dell'array è di: " +cibiPreferiti.length + " " + "elementi");
+		System.out.println("Il cibo in prima posizione è: " + cibiPreferiti[0]);
+		System.out.println("Il cibo che mi piace ma non troppo è: " + cibiPreferiti[4]);
 	}
 }
