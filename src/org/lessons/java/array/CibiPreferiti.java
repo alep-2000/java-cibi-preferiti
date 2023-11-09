e00aa16cb0e107a300801d110545e2754232db5f
alep-2000 <alessiopiras056@gmail.com>
2023-11-09 17:52:43 +0100

    Bonuses completato

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -7,5 +7,9 @@ public class CibiPreferiti {
 		System.out.println("La lunghezza dell'array è di: " +cibiPreferiti.length + " " + "elementi");
 		System.out.println("Il cibo in prima posizione è: " + cibiPreferiti[0]);
 		System.out.println("Il cibo che mi piace ma non troppo è: " + cibiPreferiti[4]);
+		
+		// BONUSES		
+		int mediano = cibiPreferiti.length / 2;
+		System.out.println("Il cibo di mezza classifica è: " + cibiPreferiti[mediano]);
 	}
 }
